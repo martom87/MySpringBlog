e5eb4bbf37460c81fdc7e5c9aa482934f4210afe
martom87 <marcintomala87@gmail.com>
2018-07-23 23:56:06 +0200

    Initial commit

---
+++ src/main/java/blog/MySpringBlogApplication.java
@@ -0,0 +1,12 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBlogApplication.class, args);
+	}
+}
---
+++ src/main/java/blog/Services/Service.java
@@ -0,0 +1,4 @@
+package blog.Services;
+
+public class Service {
+}
